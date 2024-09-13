341b16e0924377e56af9ad5956568a788cb00158
Momita Pal <palmomita75@gmail.com>
2024-09-13 21:31:16 +0530

    csv upload

--- imageprocessing/components/ImageUpload.tsx
+++ imageprocessing/components/ImageUpload.tsx
@@ -1,15 +1,18 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Player from 'lottie-react';
 import animationData from '../app/lottie/upload.json';
 
 const ImageUpload = () => {
-        const fileInputRef = useRef<HTMLInputElement>(null);
+    const [bgColor, setBgColor] = useState('rgb(246, 239, 229)');
+    const fileInputRef = useRef<HTMLInputElement>(null);
+        
 
         const handleFileChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
            const file = e.target.files?.[0];
             if(file){
                 console.log('selected file', file.name)
+                setBgColor('blanchedalmond');
             }
         }
     const handleClick = () =>{
@@ -20,24 +23,25 @@ const ImageUpload = () => {
 
     
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection:'column', paddingTop:'72px' }}>
-             <label onClick={handleClick} style={{ cursor: 'pointer' }}>
-            <div>
-                Upload Your CSV file
-            </div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection:'column', paddingTop:'72px', }}>
+             <label onClick={handleClick} style={{ cursor: 'pointer' }} >
+            
           <Player
             autoplay
             loop
             animationData={animationData} 
             style={{ height: '300px', width: '300px', paddingTop:'72px' }} 
           />
+          <div className='text-black p-[72px]  text-[38px] font-[700] rounded-[58px] top-[112px] absolute z-[-1]' style={{backgroundColor: bgColor }}>
+                Upload Your CSV file
+            </div>
           </label>
           <input
         type="file"
         accept=".csv"
         ref={fileInputRef}
         onChange={handleFileChange}
-        style={{ display: 'none' }}  // Hide the file input
+        style={{ display: 'none' }}  
       />
         </div>
   )
